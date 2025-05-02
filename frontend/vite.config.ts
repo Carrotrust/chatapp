a9ef59caaea86feaff5a54d92a9936fe756c02ab
Olamide <mac@MacBook-Pro.local>
2025-05-02 21:24:06 +0100

    updated API with render

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5500",
+        target: "https://chatapp-2p20.onrender.com",
       },
     },
   },
